d63c12d3c46b3c5e755d902a4d8c0cd133d66fac
Stefano Scipioni <scipio.it@gmail.com>
2021-10-27 21:09:42 +0200

    init

---
+++ rules.mk
@@ -0,0 +1,23 @@
+#OLED_DRIVER_ENABLE  = yes
+
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = yes
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+VIA_ENABLE = no
+LTO_ENABLE = yes # smaller size
+#AUTO_SHIFT_ENABLE = yes
+
+# https://thomasbaart.nl/2018/12/01/reducing-firmware-size-in-qmk/
+EXTRAFLAGS += -flto
+
+
+# If you want to change the display of OLED, you need to change here
+        # SRC +=  ./lib/glcdfont.c \
+        #         ./lib/rgb_state_reader.c \
+        #         ./lib/layer_state_reader.c \
+        #         ./lib/logo_reader.c \
+        #         ./lib/keylogger.c \
+        #         # ./lib/mode_icon_reader.c \
+        #         # ./lib/host_led_state_reader.c \
+#         # ./lib/timelogger.c \
